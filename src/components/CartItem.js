bcb7fffd89ce2892edfae81c1a00c359f89ebcd4
Rahimjon1995 <79519853+Rahimjon1995@users.noreply.github.com>
2021-05-20 16:53:43 +0300

    yana aldadik

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { useStateValue } from "../StateProvider";
 
 function CartItem({id, image, title, cost}) {
-    const [ dispatch] = useStateValue();
+    const [ basket,dispatch] = useStateValue();
     const removFromBasket = (e) => {
         dispatch({
             type: "REMOVE_FROM_BASKET",
             id: id,
         });
     }
+    console.log(basket);
     return (
         <div>
             <h1>{title}</h1>
